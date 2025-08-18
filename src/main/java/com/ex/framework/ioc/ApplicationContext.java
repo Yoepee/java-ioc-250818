4481bd0b4e58c4a87928de802da30e01601678b1
Yoepee <kdy2975@naver.com>
2025-08-18 15:38:21 +0900

    service red case

--- src/main/java/com/ex/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ex/framework/ioc/ApplicationContext.java
@@ -1,11 +1,25 @@
 package com.ex.framework.ioc;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ApplicationContext {
+    private final Map<String, Object> singletonObjects = new ConcurrentHashMap<>();
+    private final Map<String, Object> beanDefinitionMap = new ConcurrentHashMap<>();
+
     public ApplicationContext(String basePackage) {
 
     }
 
     public void init() {
 
+    }
+
+    public <T> T genBean(String beanName) {
+        if (singletonObjects.containsKey(beanName)) {
+            return (T) singletonObjects.get(beanName);
+        }
+
+        return null;
     }
 }
--- src/test/java/com/ex/framework/ioc/ApplicationContextTest.java
+++ src/test/java/com/ex/framework/ioc/ApplicationContextTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ApplicationContextTest {
     private static ApplicationContext applicationContext;
 
@@ -17,5 +19,14 @@ public class ApplicationContextTest {
     @DisplayName("ApplicationContext 객체 생성")
     public void t1() {
         System.out.println(applicationContext);
+    }
+
+    @Test
+    @DisplayName("testPostService 빈 얻기")
+    public void t2() {
+        TestPostService testPostService = applicationContext
+                .genBean("testPostService");
+
+        assertThat(testPostService).isNotNull();
     }
 }
